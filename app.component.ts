801d14dcf05ac76cccbd43766dba4a1e62c1d8d3
lilianwilde <37550145+lilianwilde@users.noreply.github.com>
2019-04-24 10:46:37 -0300

    alterações no componente e no html

--- app.component.ts
+++ app.component.ts
@@ -1,32 +1,38 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Meu Blog';
-  jsonPosts: Object = [];
-
+  jsonPosts: object[];
+  
 
 
   constructor () {
-  console.log(this.jsonPosts = [
+ this.jsonPosts = [
       {  
           id:1,
-          texto:"Teste1 teste teste",
-          datahora:new Date ("2019-04-04T11:25:43.511Z"). toLocaleString()
+          texto:"teste 1",
+          datahora:new Date ('2019-04-04T07:00:00').toLocaleString(),
        }, {  
           id:2,
-          texto:"Teste 2 teste  teste ",
-          datahora:new Date ("2019-04-04T12:25:43.511Z"). toLocaleString()
+          texto:"teste2",
+          datahora:new Date ('2019-05-04T07:00:00').toLocaleString(),
        }, 
        {  
           id:3,
-          texto:"Teste 3 teste  teste ",
-          datahora:new Date ("2019-04-04T12:25:43.511Z"). toLocaleString()
-       }
-    ])
+          texto:"teste3",
+          datahora:new Date ('2019-06-04T07:00:00').toLocaleString(),
+       },
+    ];
   }
-}
+  public adicionarPost(): void {
+     ;
+  };
+};
+
+
